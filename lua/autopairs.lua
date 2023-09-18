9bc8f4ccb78c2409c7a72736ad479faae7123f27
Sam-programs <None>
2023-09-18 15:21:30 +0300

    hm?

--- lua/autopairs.lua
+++ lua/autopairs.lua
@@ -384,7 +384,7 @@ local function init()
       api.nvim_feedkeys(left, "t", false)
       api.nvim_feedkeys(right, "t", false)
       local enter = api.nvim_replace_termcodes("<CR>", true, false, true)
-      api.nvim_feedkeys(enter, "in", false)
+      api.nvim_feedkeys(enter, "tn", false)
    end);
 end
 
