8b326c355acfe7b8e1347e26ddc1aef800c88acf
Sam-programs <None>
2023-09-18 18:19:15 +0300

    wrong key ..

--- lua/autopairs.lua
+++ lua/autopairs.lua
@@ -385,7 +385,7 @@ local function init()
          api.nvim_buf_set_lines(0, cursorRow + 1, cursorRow + 1, false, { newCursorLine })
          api.nvim_win_set_cursor(0, { cursorRow + 2, indentLevel + tabstop })
          local ctrlg = api.nvim_replace_termcodes("<c-g>", true, false, true)
-         api.nvim_feedkeys(ctrlg .. "U", "n", false)
+         api.nvim_feedkeys(ctrlg .. "u", "n", false)
          return
       end
       local enter = api.nvim_replace_termcodes("<CR>", true, false, true)
