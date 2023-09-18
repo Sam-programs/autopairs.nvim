6bf3e631502afc75cc8134c386e02003d47a10c7
Sam-programs <None>
2023-09-18 13:40:04 +0300

    test

--- lua/autopairs.lua
+++ lua/autopairs.lua
@@ -375,6 +375,8 @@ local function init()
          api.nvim_buf_set_lines(0, cursorRow + 1, cursorRow + 2, false, { dataAfterCursor })
       end
       -- i have no clue why i need to move the cursor back and forwards to make the indetation update for enter
+      local ctrlg = api.nvim_replace_termcodes("<c-g>", true, false, true)
+      api.nvim_feedkeys(ctrlg .. "U", "t", false)
       local right = api.nvim_replace_termcodes("<right>", true, false, true)
       local left = api.nvim_replace_termcodes("<left>", true, false, true)
       api.nvim_feedkeys(left .. right, "t", false)
